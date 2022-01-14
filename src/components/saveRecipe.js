509c15eceaa6cade4a280b5c7c09c8b9ac347ed6
Eggdad <simon.nojd.jobb@gmail.com>
2022-01-14 15:05:11 +0100

    WIP

--- src/components/saveRecipe.js
+++ src/components/saveRecipe.js
@@ -1,41 +1,147 @@
-import React from "react";
+import React, { useRef } from "react";
+import Axios from "axios";
 
-export default function saveRecipe() {
-  return (
-    <div>
-      <form>
-        <label>Receptnamn</label>
-        <br />
-        <input type="text" />
-        <br />
+class saveRecipe extends React.Component {
+  constructor(props) {
+    super(props);
 
-        <label>Kategori</label>
-        <br />
-        <input type="text" />
-        <br />
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.submit = this.submit.bind(this);
 
-        <label>Beskrivning</label>
-        <br />
-        <input type="text" />
-        <br />
+    this.state = {
+      requestbody: {
+        name: "",
+        description: "",
+        instruction: "",
+        time: 0,
+        ingredients: [
+          {
+            name: "",
+            description: "",
+          },
+        ],
+        categoryNames: [
+          {
+            name: "",
+          },
+        ],
+      },
+    };
+  }
 
-        <label>Tid</label>
-        <br />
-        <input type="text" />
-        <br />
+  render() {
+    return (
+      <div className="container p-10">
+        <form className="row g-3" onSubmit={this.onSubmit}>
+          <div className="col-md-4">
+            <label for="recipeName">Receptnamn *</label>
+            <input
+              id="recipeName"
+              value={this.state.requestbody.name}
+              className="form-control"
+              type="text"
+              onChange={(e) => this.handleInputChange(e)}
+            ></input>
+          </div>
+          <div className="col-md-4">
+            <label for="description">Beskrivning av recept *</label>
+            <input
+              id="description"
+              value={this.state.requestbody.description}
+              className="form-control"
+              type="text"
+              onChange={(e) => this.handleInputChange(e)}
+            ></input>
+          </div>
+          <div className="col-md-4">
+            <label for="instruction">Instruktioner till receptet *</label>
+            <input
+              id="instruction"
+              value={this.state.requestbody.instruction}
+              className="form-control"
+              type="text"
+              onChange={(e) => this.handleInputChange(e)}
+            ></input>
+          </div>
+          <div className="col-md-4">
+            <label for="time">Tid att tillaga *</label>
+            <input
+              id="time"
+              value={this.state.requestbody.time}
+              className="form-control"
+              type="text"
+              onChange={(e) => this.handleInputChange(e)}
+            ></input>
+          </div>
+          <div className="col-md-8">
+            <label for="ingredientName">Namn på ingrediens *</label>
+            <input
+              id="ingredients"
+              value={this.state.requestbody.ingredients.name}
+              className="form-control"
+              type="text"
+              onChange={(e) => this.handleInputChange(e)}
+            ></input>
+          </div>
+          <div className="col-md-12">
+            <label for="ingredientDescription">
+              Beskrivning av ingrediens *
+            </label>
+            <input
+              id="ingredientDescription"
+              value={this.state.requestbody.ingredients.description}
+              className="form-control"
+              type="text"
+              onChange={(e) => this.handleInputChange(e)}
+            ></input>
+          </div>
+          <div style={{ display: "flex", justifyContent: "center" }}>
+            <button>Avbryt</button>
+            <button onClick={this.submit()}>Spara recept</button>
+          </div>
+        </form>
+      </div>
+    );
+  }
 
-        <label>Ingrediens</label>
-        <br />
-        <input type="text" />
-        <br />
+  onSubmit(e) {
+    e.preventDefault();
+  }
 
-        <label>Instruktioner</label>
-        <br />
-        <input type="text" />
-        <br />
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.value;
+    const name = target.id;
 
-        <input type="submit" value="Spara" />
-      </form>
-    </div>
-  );
+    this.setState({
+      requestbody: {
+        ...this.state.requestbody,
+        [name]: value,
+      },
+    });
+  }
+
+  async submit() {
+    const requestbody = this.state.requestbody;
+
+    await Axios.post("http://localhost:8080/v1/recipe/save", {
+      name: requestbody.name,
+      description: requestbody.description,
+      instruction: requestbody.instruction,
+      time: requestbody.time,
+      ingredients: [
+        {
+          name: requestbody.ingredients.name,
+          description: requestbody.ingredients.description,
+        },
+      ],
+      categoryNames: [
+        {
+          name: requestbody.categoryNames.name,
+        },
+      ],
+    });
+  }
 }
+
+export default saveRecipe;
