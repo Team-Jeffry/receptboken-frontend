e65209807d4fd0cc336479c5d7bb7c78784675b4
baliharko <bali@fauxdelorean.com>
2022-01-15 11:10:53 +0100

    made scrollable version

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Link, animateScroll as scroll } from "react-scroll";
 import Suggest from "./component/Suggest";
 import Home from "./component/Home";
 
 function App() {
     return (
-        <Router>
-            <Routes>
-                <Route exact path="/" element={<Home />} />
-                <Route exact path="/suggest" element={<Suggest />} />
-            </Routes>
-        </Router>
+        <div>          
+            <Home id="home"/>
+            <Suggest id="suggest"/>
+        </div>
     );
 }
 
--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,14 +1,14 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
+import { Link, animateScroll as scroll } from "react-scroll";
 
 export default class Home extends Component {
     render() {
         return (
-            <div className="background">
+            <div className="home">
                 <h1 className="title">Receptboken</h1>
                 <div className="container">
                     <button>Sök recept</button>
-                    <Link to="/suggest">
+                    <Link activeClass="active" to="suggest" spy={true} smooth={true} offset={0} duration={900}>
                         <button>Laga med det jag har</button>
                     </Link>
                     <button>Skapa nytt recept</button>
@@ -17,3 +17,4 @@ export default class Home extends Component {
         );
     }
 }
+
