dc0c21c8e1d8a406de38d08b664db8a005b54f86
lisaramel <lisa.ramel@yh.nackademin.se>
2022-01-17 12:52:17 +0100

    Working on modal

--- src/components/ShowRecipe.jsx
+++ src/components/ShowRecipe.jsx
@@ -6,56 +6,67 @@ import Button from "react-bootstrap/Button";
 class ShowRecipe extends Component {
   constructor() {
     super();
-  this.state = {
-    showHide: false,
-    recipe: {
-      name: "",
+    this.state = {
+      showHide: false,
+      recipe: {
+        /* name: "bla",
       description: "",
       instruction: "",
       time: "",
       ingredients: [],
-      categories: [],
-    },
-  };
-}
+      categories: [],*/
+      },
+    };
+  }
 
-  componentDidMount() {
+  componentDidMount = async () => {
     const postData = {
       recipeName: "Fruktsallad med Jordgubbar",
       categoryNames: ["Frukt"],
       time: 12,
     };
 
-    axios.post("http://localhost:8080/v1/recipe/get", postData).then((res) => {
-      console.log(res);
-      //const recipe = res.data;
-      this.setState({ name: res.data.name });
-      console.log(res.data.name);
-    });
-  }
+    await axios
+      .post("http://localhost:8080/v1/recipe/get", postData)
+      .then((res) => {
+        console.log(res);
+
+        this.res = res.data;
+        this.state.recipe = this.res[0];
+        return this.state.recipe;
+      });
+  };
 
   handleModalShowHide() {
-    this.setState({ showHide: !this.state.showHide })
+    this.setState({ showHide: !this.state.showHide });
   }
 
   render() {
     return (
       <div>
-      <Button variant="primary" onClick={() => this.handleModalShowHide()}>
+        <Button variant="primary" onClick={() => this.handleModalShowHide()}>
           Launch demo modal
-      </Button>
+        </Button>
 
-      <Modal show={this.state.showHide}>
+        <Modal show={this.state.showHide}>
           <Modal.Header closeButton onClick={() => this.handleModalShowHide()}>
-          <Modal.Title id="name">{this.state.name}</Modal.Title>
+            <Modal.Title className="modal-title">{this.state.recipe.name}</Modal.Title>
           </Modal.Header>
-          <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+          <Modal.Body>
+            <div className="container">
+            <div>{this.state.recipe.description}</div>
+
+              <div className="row">
+                <div className="col"></div>
+                <div className="col">{this.state.recipe.instruction}</div>
+              </div>
+            </div>
+          </Modal.Body>
           <Modal.Footer>
 
           </Modal.Footer>
-      </Modal>
-
-  </div>
+        </Modal>
+      </div>
     );
   }
 }
