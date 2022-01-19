7796de0d0a3ac2b5d46588c2cdcdb52e9d4db614
baliharko <bali@fauxdelorean.com>
2022-01-19 17:45:46 +0100

    small fix to blurring issue on firefox

--- src/component/SearchRecipe.jsx
+++ src/component/SearchRecipe.jsx
@@ -4,159 +4,155 @@ import Axios from "axios";
 import SuggestionList from "./SuggestionList";
 
 const keyCodes = {
-  comma: 188,
-  enter: [10, 13],
+    comma: 188,
+    enter: [10, 13],
 };
 
 const delimiters = [...keyCodes.enter, keyCodes.comma];
 
 class SearchRecipe extends React.Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.handleDeleteCategory = this.handleDeleteCategory.bind(this);
-    this.handleAdditionCategory = this.handleAdditionCategory.bind(this);
-    this.handleDrag = this.handleDrag.bind(this);
-    this.handleInputChange = this.handleInputChange.bind(this);
-    this.submit = this.submit.bind(this);
+        this.handleDeleteCategory = this.handleDeleteCategory.bind(this);
+        this.handleAdditionCategory = this.handleAdditionCategory.bind(this);
+        this.handleDrag = this.handleDrag.bind(this);
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.submit = this.submit.bind(this);
 
-    this.state = {
-      categoryTags: [],
-      categorySuggestions: [],
-      suggestionResults: [],
-      saveRecipeJson: {
-        name: "",
-        time: "",
-        categoryNames: [],
-      },
-    };
-  }
+        this.state = {
+            categoryTags: [],
+            categorySuggestions: [],
+            suggestionResults: [],
+            saveRecipeJson: {
+                name: "",
+                time: "",
+                categoryNames: [],
+            },
+        };
+    }
 
-  handleInputChange(event) {
-    const target = event.target;
-    const value = target.value;
-    const name = target.id;
+    handleInputChange(event) {
+        const target = event.target;
+        const value = target.value;
+        const name = target.id;
 
-    this.setState({
-      saveRecipeJson: {
-        ...this.state.saveRecipeJson,
-        [name]: value,
-      },
-    });
-  }
-
-  async submit() {
-    const categories = [];
-
-    this.state.categoryTags.forEach((element) => {
-      categories.push(element.text);
-    });
-
-    const requestBody = {
-      recipeName: this.state.saveRecipeJson.name,
-      categoryNames: categories,
-      time: this.state.saveRecipeJson.time,
-    };
-
-    await Axios.post("http://localhost:8080/v1/recipe/get", requestBody)
-      .then((response) => {
-        this.setState({ suggestionResults: response.data });
-        console.log(this.state.suggestionResults);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }
-
-  async componentDidMount() {
-    await Axios.get("http://localhost:8080/v1/category/all").then(
-      (response) => {
-        const categories = response.data.map((element) => {
-          return {
-            id: element.name,
-            text: element.name,
-          };
+        this.setState({
+            saveRecipeJson: {
+                ...this.state.saveRecipeJson,
+                [name]: value,
+            },
         });
-        this.setState({ categorySuggestions: categories });
-      }
-    );
-  }
+    }
 
-  handleDeleteCategory(i) {
-    const { categoryTags } = this.state;
-    this.setState({
-      categoryTags: categoryTags.filter((tag, index) => index !== i),
-    });
-  }
+    async submit() {
+        const categories = [];
 
-  handleAdditionCategory(tag) {
-    this.setState((state) => ({ categoryTags: [...state.categoryTags, tag] }));
-  }
+        this.state.categoryTags.forEach((element) => {
+            categories.push(element.text);
+        });
 
-  handleDrag(tag, currPos, newPos) {
-    const tags = [...this.state.tags];
-    const newTags = tags.slice();
+        const requestBody = {
+            recipeName: this.state.saveRecipeJson.name,
+            categoryNames: categories,
+            time: this.state.saveRecipeJson.time,
+        };
 
-    newTags.splice(currPos, 1);
-    newTags.splice(newPos, 0, tag);
+        await Axios.post("http://localhost:8080/v1/recipe/get", requestBody)
+            .then((response) => {
+                this.setState({ suggestionResults: response.data });
+                console.log(this.state.suggestionResults);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }
 
-    // re-render
-    this.setState({ tags: newTags });
-  }
+    async componentDidMount() {
+        await Axios.get("http://localhost:8080/v1/category/all").then((response) => {
+            const categories = response.data.map((element) => {
+                return {
+                    id: element.name,
+                    text: element.name,
+                };
+            });
+            this.setState({ categorySuggestions: categories });
+        });
+    }
 
-  render() {
-    const categoryTags = this.state.categoryTags;
-    const categorySuggestions = this.state.categorySuggestions;
-    const suggestionResults = this.state.suggestionResults;
+    handleDeleteCategory(i) {
+        const { categoryTags } = this.state;
+        this.setState({
+            categoryTags: categoryTags.filter((tag, index) => index !== i),
+        });
+    }
 
-    return (
-      <div>
-        <div className="search-recipe">
-          <div className="blurredBackground"></div>
-          <div className="container">
-            <div className="title-smaller">
-              <h1>Sök efter recept</h1>
+    handleAdditionCategory(tag) {
+        this.setState((state) => ({ categoryTags: [...state.categoryTags, tag] }));
+    }
+
+    handleDrag(tag, currPos, newPos) {
+        const tags = [...this.state.tags];
+        const newTags = tags.slice();
+
+        newTags.splice(currPos, 1);
+        newTags.splice(newPos, 0, tag);
+
+        // re-render
+        this.setState({ tags: newTags });
+    }
+
+    render() {
+        const categoryTags = this.state.categoryTags;
+        const categorySuggestions = this.state.categorySuggestions;
+        const suggestionResults = this.state.suggestionResults;
+
+        return (
+            <div>
+                <div className="search-recipe">
+                    <div className="blurredBackground" />
+                    <div className="container">
+                        <div className="title-smaller">
+                            <h1>Sök efter recept</h1>
+                        </div>
+                        <form>
+                            <input
+                                onChange={(e) => this.handleInputChange(e)}
+                                type="text"
+                                id="name"
+                                value={this.state.saveRecipeJson.name}
+                                placeholder="Namn på recept"
+                            />
+                            <br />
+                            <input
+                                onChange={(e) => this.handleInputChange(e)}
+                                type="number"
+                                id="time"
+                                value={this.state.saveRecipeJson.time}
+                                placeholder="Tillagningstid"
+                            />
+                        </form>
+                        <ReactTags
+                            inputFieldPosition="top"
+                            allowDragDrop={true}
+                            allowUnique={true}
+                            placeholder="Kategori"
+                            suggestions={categorySuggestions}
+                            tags={categoryTags}
+                            handleDelete={this.handleDeleteCategory}
+                            handleAddition={this.handleAdditionCategory}
+                            delimiters={delimiters}
+                        />
+                        <br />
+                        <button style={{ margin: "20px" }} onClick={this.submit}>
+                            Sök
+                        </button>
+                    </div>
+                    {this.state.categoryTags.length !== 0 && <SuggestionList data={suggestionResults} />}
+                </div>
             </div>
-            <form>
-              <input
-                onChange={(e) => this.handleInputChange(e)}
-                type="text"
-                id="name"
-                value={this.state.saveRecipeJson.name}
-                placeholder="Namn på recept"
-              />
-              <br />
-              <input
-                onChange={(e) => this.handleInputChange(e)}
-                type="number"
-                id="time"
-                value={this.state.saveRecipeJson.time}
-                placeholder="Tillagningstid"
-              />
-            </form>
-            <ReactTags
-              inputFieldPosition="top"
-              allowDragDrop={true}
-              allowUnique={true}
-              placeholder="Kategori"
-              suggestions={categorySuggestions}
-              tags={categoryTags}
-              handleDelete={this.handleDeleteCategory}
-              handleAddition={this.handleAdditionCategory}
-              delimiters={delimiters}
-            />
-            <br />
-            <button style={{ margin: "20px" }} onClick={this.submit}>
-              Sök
-            </button>
-          </div>
-          {this.state.categoryTags.length !== 0 && (
-            <SuggestionList data={suggestionResults} />
-          )}
-        </div>
-      </div>
-    );
-  }
+        );
+    }
 }
 
 export default SearchRecipe;
