917068f6515faf2149da97ccc8aa2e5a5435b7d2
baliharko <bali@fauxdelorean.com>
2022-01-19 17:07:54 +0100

    #1v8ah6z cleanup in home section on lower resolution screens

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,44 +1,28 @@
 import React, { Component } from "react";
 import { Link } from "react-scroll";
 
+const style = {
+    display: "flex",
+    justifyContent: "center",
+};
+
 export default class Home extends Component {
-  render() {
-    return (
-      <div ref={this.myRef} className="home">
-        <h1 className="title">Receptboken</h1>
-        <div className="container">
-          <Link
-            activeClass="active"
-            to="search-recipe"
-            spy={true}
-            smooth={true}
-            offset={-200}
-            duration={900}
-          >
-            <button>Sök recept</button>
-          </Link>
-          <Link
-            activeClass="active"
-            to="suggest"
-            spy={true}
-            smooth={true}
-            offset={-200}
-            duration={900}
-          >
-            <button>Laga med det jag har</button>
-          </Link>
-          <Link
-            activeClass="active"
-            to="save-recipe"
-            spy={true}
-            smooth={true}
-            offset={-200}
-            duration={900}
-          >
-            <button>Skapa nytt recept</button>
-          </Link>
-        </div>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div ref={this.myRef} className="home">
+                <h1 className="title">Receptboken</h1>
+                <div style={style}>
+                    <Link activeClass="active" to="search-recipe" spy={true} smooth={true} offset={0} duration={900}>
+                        <button>Sök recept</button>
+                    </Link>
+                    <Link activeClass="active" to="suggest" spy={true} smooth={true} offset={-280} duration={900}>
+                        <button>Laga med det jag har</button>
+                    </Link>
+                    <Link activeClass="active" to="save-recipe" spy={true} smooth={true} offset={0} duration={900}>
+                        <button>Skapa nytt recept</button>
+                    </Link>
+                </div>
+            </div>
+        );
+    }
 }
